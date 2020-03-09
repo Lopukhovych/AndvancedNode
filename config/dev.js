d174172aae5e3efdefa85d89cfca40fca418e238
volodymyrlopukhovych <volodymyr.lopukhovychh@gmail.com>
2020-03-09 21:50:03 +0200

    fixed data

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
 	googleClientID:
-		'964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-	googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+		'643744474669-7d740irufvcbkrrgo8uotiba884gg3dm.apps.googleusercontent.com',
+	googleClientSecret: 'DAefvEbCX5VSQsMfmQ9qIi_d',
 	// mongoURI: `mongodb://volod34:xYTCuFHvCTB6JRpm@cluster0-shard-00-00-bfdbl.mongodb.net:27017,cluster0-shard-00-01-bfdbl.mongodb.net:27017,cluster0-shard-00-02-bfdbl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`,
 	mongoURI: `mongodb://127.0.0.1:27017/advanced`,
 	redisURI: 'redis://127.0.0.1:6379',
